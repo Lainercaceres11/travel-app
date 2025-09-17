974595d01246913dc1c44bb771d144d6e4bff35e
Lainercaceres11 <lainercaceres12@gmail.com>
2025-09-17 14:51:04 -0500

    change import prismaClient

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@/src/generated/prisma";
-
+import { PrismaClient } from "../src/generated/prisma";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
